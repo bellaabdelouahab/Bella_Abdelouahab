5dc6cd3c24fed04f6e9e3e5afdbc379c26ac001e
Abdelouahab Bella <abdobella977@gmail.com>
2022-11-28 08:49:15 +0100

    fin Question 3

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 
 //requirements
+const express = require('express')
+
 
 const app=express();
 
 //utiliser les middleware nissecaires
 
+
 //Definition des routes 
- 
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+
 
 module.exports=app
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 //requirements
-
+const app = require('./app')
+const db=require('mongoose')
+require('dotenv').config()
 //dfinition du moteur de template
-
+app.set('view engine', 'pug')
 
 // Etablire une connexion à la base de données
-
+db.connect(process.env.DB_URI,{useNewUrlParser:true})
 
 //Démarage du serveur su le le port : PORT dans .env
+const server = app.listen(process.env.PORT || 3000,()=>{
+    console.log('api listeningat : http://localhost:3000') 
+    
+})
