2199a3e09ad085481a1bcf44e1b8d7190f0fe1b7
Abdelouahab Bella <abdobella977@gmail.com>
2022-11-28 08:52:33 +0100

    fin Question 4

--- models/post/postModel.js
+++ models/post/postModel.js
@@ -5,17 +5,30 @@ const mongoose=require("mongoose")
 const postSchema=mongoose.Schema({
     titre: {
        // chaine de caractère unique non nulle
+        type:String,
+        required:true,
+        unique:true      
     },
     auteur:{
         // chaine de caractère avec la valeur "unkown" par defaut
+        type:String,
+        default:"unkown"
     },
     resume:{
         // chaine de caractère non nulle de moins de 100 caractères
-    },
+        type:String,
+        required:true,
+        maxlength:100
+    }, 
 
     content: {
         // chaine de caractère non nulle, au moins de 100 caractères
+        type:String,
+        required:true,
+        minlength:100
     }
 }, {timestamps:true})
 
 //créer et exporter un Modéle Post
+
+module.exports=mongoose.model("Post",postSchema)
